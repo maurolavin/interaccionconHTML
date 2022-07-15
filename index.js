4d27b7a2a898bb4a01518770336a0b1939059d6f
mauro.lavin <villarroelmauroemanuel@gmail.com>
2022-07-14 23:30:13 -0300

    arreglo precios

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ if(producto === "aceite"){
 
 const noEncontrado = 'Producto no encontrado';
 
-const productos =[ "aceite de oliva extra virgen x500cc $800",
-                "nueces californianas peladas por kilogramo $1800",
+const productos =[ "aceite de oliva extra virgen x500cc $650",
+                "nueces californianas peladas por kilogramo $1500",
                 "vino uva bonarda cosecha 2022 $1200",
 ]
 
@@ -42,7 +42,7 @@ if(eleccion === "comprar"){
             alert(productos[0]);
         }else if(producto === "nueces"){
             alert(productos[1]);;
-        }else if(producto === "vino"){
+        }else if(producto = "vino"){
             alert(productos[2]);;
         }else{
             alert("Producto no encontrado");
