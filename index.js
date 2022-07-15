c67bd9e1e0f61918936ffb56ba11fbcc59e5bef0
mauro.lavin <villarroelmauroemanuel@gmail.com>
2022-07-14 23:32:47 -0300

    minor fixes

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-/* const noEncontrado = 'Producto no encontrado';
-
-const productos =[ "aceite de oliva extra virgen x500cc $800",
-                "nueces californianas peladas por kilogramo $1800",
-                "vino uva bonarda cosecha 2022 $1200",
-]
-
-let producto = prompt("¡Hola, Bienvenido a Finca Tierras Catamarqueñas! Tenemos aceite de oliva, vinos y nueces. Que desea comprar?");
-
-if(producto === "aceite"){
-    alert(productos[0]);
-}else if(producto === "nueces"){
-    alert(productos[1]);;
-}else if(producto === "vino"){
-    alert(productos[2]);;
-}else{
-    alert("Producto no encontrado");
-} */
-
 const noEncontrado = 'Producto no encontrado';
 
 const productos =[ "aceite de oliva extra virgen x500cc $650",
