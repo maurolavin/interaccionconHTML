8d167594a4ba0a9dfbd798a29b0660012f55fed3
mauro.lavin <villarroelmauroemanuel@gmail.com>
2022-07-19 23:50:05 -0300

    INTERACCION CON HTML

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ const reclamos =[
     "Para iniciar el reclamo, contactese a reclamos@fincatierras.com"
 ]
 
-let eleccion = prompt("¡Hola, Bienvenido a Finca Tierras Catamarqueñas! Desea comprar, contactarnos o iniciar un reclamo?");
+let seccion = prompt("¡Hola, Bienvenido a Finca Tierras Catamarqueñas! Desea comprar, contactar o reclamar?");
+/* let imgContainer = document.getElementById("imagenContainer");
 
 if(eleccion === "comprar"){
-    let producto = prompt("Tenemos aceite, nueces y vinos. Que desea comprar?")
+    let producto = prompt("Tenemos aceite, nueces y vinos. Que desea comprar?");
         if(producto === "aceite"){
             alert(productos[0]);
         }else if(producto === "nueces"){
@@ -32,14 +33,24 @@ if(eleccion === "comprar"){
     alert(contactos);
 }else if(eleccion = "reclamo"){
     alert(reclamos);
+}  
+if(producto === "aceite"){
+    imagenContainer.className = "imgcontainergreen";
+};
+ */
+let bienvenida = document.getElementById("bienvenida");
+if(seccion === "comprar"){
+    bienvenida.innerHTML = "<h4>Tenemos aceite de oliva extra virgen y nueces californianas de la mas alta calidad</h4>";
+    bienvenida.className = "h4green";
+}else if(seccion === "contactar"){
+    bienvenida.innerHTML = "<h4>Podes contactarnos en instagram @fincatierras, facebook.com/fincatierrasc o nuestra tienda en mercadolibre</h4>";
+    bienvenida.className = "h4black";
+}else if(seccion === "reclamar"){
+    bienvenida.innerHTML = "<h4>Para reclamos, devoluciones o sugerencias, por favor contactanos en info@fincatierras.com.ar con el asunto reclamo</h4>";
+    bienvenida.className = "h4"
+}else {
+    bienvenida.innerHTML = "<h4>Bienvenido, que estas buscando?</h4>";
 }
-/* }else if(producto === "nueces"){
-    alert(productos[1]);;
-}else if(producto === "vino"){
-    alert(productos[2]);;
-}else{
-    alert("Producto no encontrado");
-} */
 
 
 
